cdd5d84bb36d766514373db16a3d5697c0f5f09c
phillychi3 <phillychi3@gmail.com>
2020-03-03 20:20:53 +0800

    Update nhen-tai.py

--- nhen-tai.py
+++ nhen-tai.py
@@ -1,48 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import tkinter as tk
-import sys
-import webbrowser
-chromepath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chromepath))
-window = tk.Tk() #初始化視窗
-# 設定視窗標題、大小和背景顏色
-window.title('網頁快速瀏覽器')
-window.geometry('200x100')
-window.configure(background='white')
-
-header_label = tk.Label(window, text='數字快速瀏覽器')
-header_label.pack()
 
 
-
-e = tk.Entry(window, show = None)#顯示成明文形式
-e.pack()
-
-def nhentai():
-    number = str(e.get())
-    print(number)
-    print("hi")
-    url = ("nhentai.net/g/" + number)
-    webbrowser.get('chrome').open(url)
-    
-
-
-
-# 以下為 weight_frame 群組
-
-
-
-calculate_btn = tk.Button(window, text='馬上前往', command=nhentai)
-calculate_btn.pack()
-
-
-
-
-
-    
-window.mainloop()
-=======
 import tkinter as tk
 import sys
 import webbrowser
@@ -85,48 +42,4 @@ calculate_btn.pack()
 
     
 window.mainloop()
->>>>>>> 82e718fbde66058f607f0aa20e44db09bdc9b994
-=======
-import tkinter as tk
-import sys
-import webbrowser
-chromepath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chromepath))
-window = tk.Tk() #初始化視窗
-# 設定視窗標題、大小和背景顏色
-window.title('網頁快速瀏覽器')
-window.geometry('200x100')
-window.configure(background='white')
 
-header_label = tk.Label(window, text='數字快速瀏覽器')
-header_label.pack()
-
-
-
-e = tk.Entry(window, show = None)#顯示成明文形式
-e.pack()
-
-def nhentai():
-    number = str(e.get())
-    print(number)
-    print("hi")
-    url = ("nhentai.net/g/" + number)
-    webbrowser.get('chrome').open(url)
-    
-
-
-
-
-
-
-
-calculate_btn = tk.Button(window, text='馬上前往', command=nhentai)
-calculate_btn.pack()
-
-
-
-
-
-    
-window.mainloop()
->>>>>>> 82e718fbde66058f607f0aa20e44db09bdc9b994
